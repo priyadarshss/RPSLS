4925e41cd09665bcbb6756fb1507e92079f4488d
Priyadarsh S S <priyadarsh2001@gmail.com>
2024-01-02 03:19:12 +0530

    Add remotePatterns to next.config.js and update dependencies in package.json

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 function RPSGame() {
@@ -7,7 +8,15 @@ function RPSGame() {
         <h1 className='text-6xl font-normal mb-8 leading-normal'>
           Rock, Paper, Scissors, {<br />} Lizard & Spock
         </h1>
-        <div className='mt-40'>
+        <Image
+          src='https://rpsls-game.vercel.app/assets/img/RPSLS.png'
+          alt='Rock, Paper, Scissors, Lizard & Spock'
+          width={300}
+          height={100}
+          priority
+          className='hover:scale-110 transition duration-300 ease-in-out opacity-80 hover:opacity-100'
+        />
+        <div className='mt-20'>
           <Link href='/start'>
             <button className='bg-[#8575FF] text-white p-4 m-4 rounded-full hover:bg-[#6145ff] transition duration-300 ease-in-out'>
               Start New Game
@@ -16,11 +25,6 @@ function RPSGame() {
           <Link href='/join'>
             <button className='bg-[#8575FF] text-white p-4 m-4 rounded-full hover:bg-[#6145ff] transition duration-300 ease-in-out'>
               Join Existing Game
-            </button>
-          </Link>
-          <Link href='/continue'>
-            <button className='bg-[#8575FF] text-white p-4 m-4 rounded-full hover:bg-[#6145ff] transition duration-300 ease-in-out'>
-              Continue Game
             </button>
           </Link>
         </div>
